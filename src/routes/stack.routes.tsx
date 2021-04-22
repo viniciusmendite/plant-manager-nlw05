246da1fa4ccc0653e8d38338ea05f3f4b68a4e10
Vinicius Mendite <vinicius.miff@gmail.com>
2021-04-22 16:20:27 -0300

    create bottom tabs

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Confirmation } from '../pages/Confirmation';
 import { PlantSelect } from '../pages/PlantSelect';
 import { PlantSave } from '../pages/PlantSave';
 import { MyPlants } from '../pages/MyPlants';
+import AuthRoutes from './tab.routes';
 
 const stackRoutes = createStackNavigator();
 
@@ -23,9 +24,9 @@ export const AppStackRoutes: React.FC = () => {
       <stackRoutes.Screen name="Welcome" component={Welcome} />
       <stackRoutes.Screen name="UserIdentification" component={UserIdentification} />
       <stackRoutes.Screen name="Confirmation" component={Confirmation} />
-      <stackRoutes.Screen name="PlantSelect" component={PlantSelect} />
+      <stackRoutes.Screen name="PlantSelect" component={AuthRoutes} />
       <stackRoutes.Screen name="PlantSave" component={PlantSave} />
-      <stackRoutes.Screen name="MyPlants" component={MyPlants} />
+      <stackRoutes.Screen name="MyPlants" component={AuthRoutes} />
     </stackRoutes.Navigator>
   );
 }
